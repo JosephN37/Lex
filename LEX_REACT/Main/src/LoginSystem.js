cdd6ba62f106ee26c054476d3a46198aa95ca8a1
Joseph Nathanael <josephnathanael.2014@gmail.com>
2021-06-23 12:06:05 +0800

    progress in userdashboard page

--- LEX_REACT/Main/src/LoginSystem.js
+++ LEX_REACT/Main/src/LoginSystem.js
@@ -1,23 +1,25 @@
 
-import { FirebaseAuthProvider, FirebaseAuthConsumer, IfFirebaseAuthed,
-    IfFirebaseUnAuthed } from '@react-firebase/auth';
+import {
+    FirebaseAuthProvider, FirebaseAuthConsumer, IfFirebaseAuthed,
+    IfFirebaseUnAuthed
+} from '@react-firebase/auth';
 import { config } from './config/firebase';
 import { firebase } from "@firebase/app";
 import "@firebase/auth";
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import LoginPage from "./LoginPage";
 import RegisterPage from './RegisterPage';
+import UserDashboard from './UserDashboard';
 
 /*
 * A loginSystem component, passing props to LoginPage.js and RegisterPage.js, handling authentication 
 */
 function LoginSystem() {
-    
     // function to handle email login
-    function handleEmailLogIn(){
+    function handleEmailLogIn() {
         clearErrors();
         firebase.auth().signInWithEmailAndPassword(email, password).catch((err) => {
-            switch (err.code){
+            switch (err.code) {
                 case "auth/invalid-email":
                 case "auth/user-disabled":
                 case "auth/user-not-found":
@@ -29,58 +31,65 @@ function LoginSystem() {
             }
         });
     }
-    
+
     // function to handle email password signup
-    function handleSignUp(){
+    function handleSignUp() {
         clearErrors();
         firebase.auth().createUserWithEmailAndPassword(email, password).catch((err) => {
-            if(confirmPassword !== password){
-            setPasswordError('Passwords do not match');
-            
+            if (confirmPassword !== password) {
+                setPasswordError('Passwords do not match');
+
             } else {
-            switch (err.code){
-                case "auth/email-already-in-use":
-                case "auth/invalid-email":
-                    console.log(err.message);
-                    setEmailError(err.message);
-                    break;
-                case "auth/weak-password":
-                    console.log(err.message);
-                    setPasswordError(err.message);
-                    break;
+                switch (err.code) {
+                    case "auth/email-already-in-use":
+                    case "auth/invalid-email":
+                        console.log("error message: " + err.message);
+                        setEmailError(err.message);
+                        break;
+                    case "auth/weak-password":
+                        console.log(err.message);
+                        setPasswordError("error message: " + err.message);
+                        break;
+                }
             }
-        }});
-        
+        });
+
     }
 
     //Setting up the states 
-    const[confirmPassword,setConfirmPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
     const [user, setUser] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [emailError, setEmailError] = useState("");
     const [passwordError, setPasswordError] = useState("");
     const [hasAccount, setHasAccount] = useState(false);
+    const [userProfileName, setUserProfileName] = useState("");
+    const [isNewUser, setIsNewUser] = useState(true);
+
 
     // function to clear email, password, and confirmPassword state
-    const clearInputs = () =>{
+    const clearInputs = () => {
         setEmail("");
         setPassword("");
         setConfirmPassword("");
     }
 
     // function to clear email error and password error states
-    const clearErrors = () =>{
+    const clearErrors = () => {
         setEmailError("");
         setPasswordError("");
     }
 
     // function which detects a state change regarding authentication (whether user has signed up / logged in / logged out), 
     // and modifies the user state accordingly
-    const authListener = () =>{
-        firebase.auth().onAuthStateChanged((user) =>{
-            if (user){
+    const authListener = () => {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
                 clearInputs();
+                // console.log("user: " + user);
+                // console.log("a: " + user.additionalUserInfo.isNewUser);
+                // setIsNewUser(user.additionalUserInfo.isNewUser);
                 setUser(user);
             } else {
                 setUser("");
@@ -94,38 +103,40 @@ function LoginSystem() {
     }, []);
 
     return (
-        user ? (<button className="btn btn-lg btn-outline-dark" onClick={()=>{firebase.app().auth().signOut()}}>
-        Log Out
-      </button> ) : (
-        hasAccount ? (
-        <LoginPage 
-        email = {email}
-        setEmail= {setEmail}
-        password = {password}
-        setPassword = {setPassword}
-        handleEmailLogIn = {handleEmailLogIn}
-        handleSignUp = {handleSignUp}
-        hasAccount = {hasAccount}
-        setHasAccount = {setHasAccount}
-        emailError = {emailError}
-        passwordError = {passwordError}
-        confirmPassword = {confirmPassword}
-        setConfirmPassword={setConfirmPassword}> </LoginPage>
-        ) : (
-        <RegisterPage 
-        email = {email}
-        setEmail= {setEmail}
-        password = {password}
-        setPassword = {setPassword}
-        handleEmailLogIn = {handleEmailLogIn}
-        handleSignUp = {handleSignUp}
-        hasAccount = {hasAccount}
-        setHasAccount = {setHasAccount}
-        emailError = {emailError}
-        passwordError = {passwordError}
-        confirmPassword = {confirmPassword}
-        setConfirmPassword={setConfirmPassword}> </RegisterPage>
-        ))
+        user ? <UserDashboard /> : (
+            hasAccount ? (
+                <LoginPage
+                    email={email}
+                    setEmail={setEmail}
+                    password={password}
+                    setPassword={setPassword}
+                    handleEmailLogIn={handleEmailLogIn}
+                    handleSignUp={handleSignUp}
+                    hasAccount={hasAccount}
+                    setHasAccount={setHasAccount}
+                    emailError={emailError}
+                    passwordError={passwordError}
+                    confirmPassword={confirmPassword}
+                    setConfirmPassword={setConfirmPassword}
+                    userProfileName={userProfileName}
+                    setUserProfileName={setUserProfileName}> </LoginPage>
+            ) : (
+                <RegisterPage
+                    email={email}
+                    setEmail={setEmail}
+                    password={password}
+                    setPassword={setPassword}
+                    handleEmailLogIn={handleEmailLogIn}
+                    handleSignUp={handleSignUp}
+                    hasAccount={hasAccount}
+                    setHasAccount={setHasAccount}
+                    emailError={emailError}
+                    passwordError={passwordError}
+                    confirmPassword={confirmPassword}
+                    setConfirmPassword={setConfirmPassword}
+                    userProfileName={userProfileName}
+                    setUserProfileName={setUserProfileName}> </RegisterPage>
+            ))
     );
 }
 
--- LEX_REACT/Main/src/config/firebase.js
+++ LEX_REACT/Main/src/config/firebase.js
@@ -1,13 +1,14 @@
 import { firebase } from "@firebase/app";
 
 export const config = {
-    apiKey: "AIzaSyBGbsBGUtxmXCgu2y-qq36jEiPoNQwHJm0",
-    authDomain: "lex-login-page.firebaseapp.com",
-    projectId: "lex-login-page",
-    storageBucket: "lex-login-page.appspot.com",
-    messagingSenderId: "721065248225",
-    appId: "1:721065248225:web:df32c6b7e0a6e093f53ea0",
-    measurementId: "G-V2MKT2QE1X",
-    databaseURL: "https://lex-login-page-default-rtdb.asia-southeast1.firebasedatabase.app"
-  };
+  apiKey: "AIzaSyBGbsBGUtxmXCgu2y-qq36jEiPoNQwHJm0",
+  authDomain: "lex-login-page.firebaseapp.com",
+  databaseURL: "https://lex-login-page-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "lex-login-page",
+  storageBucket: "lex-login-page.appspot.com",
+  messagingSenderId: "721065248225",
+  appId: "1:721065248225:web:df32c6b7e0a6e093f53ea0",
+  measurementId: "G-V2MKT2QE1X"
+};
+
 
