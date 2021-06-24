932dfb916c35e689f14502f18c0e0623f8c1c6b8
aryagiat <arya.giat@gmail.com>
2021-06-24 10:23:20 +0800

    style: fix styling

--- LEX_REACT/Main/src/LoginSystem.js
+++ LEX_REACT/Main/src/LoginSystem.js
@@ -1,118 +1,135 @@
-
-import { FirebaseAuthProvider, FirebaseAuthConsumer, IfFirebaseAuthed,
-    IfFirebaseUnAuthed } from '@react-firebase/auth';
-import { config } from './config/firebase';
+import {
+  FirebaseAuthProvider,
+  FirebaseAuthConsumer,
+  IfFirebaseAuthed,
+  IfFirebaseUnAuthed,
+} from "@react-firebase/auth";
+import { config } from "./config/firebase";
 import { firebase } from "@firebase/app";
 import "@firebase/auth";
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import LoginPage from "./LoginPage";
-import RegisterPage from './RegisterPage';
+import RegisterPage from "./RegisterPage";
 
 function LoginSystem() {
-    
-    function handleEmailLogIn(){
-        clearErrors();
-        firebase.auth().signInWithEmailAndPassword(email, password).catch((err) => {
-            switch (err.code){
-                case "auth/invalid-email":
-                case "auth/user-disabled":
-                case "auth/user-not-found":
-                    setEmailError(err.message);
-                    break;
-                case "auth/wrong-password":
-                    setPasswordError(err.message);
-                    break;
-            }
-        });
-    }
-    
-    function handleSignUp(){
-        clearErrors();
-        firebase.auth().createUserWithEmailAndPassword(email, password).catch((err) => {
-            if(confirmPassword !== password){
-            setPasswordError('Passwords do not match');
-            
-            } else {
-            switch (err.code){
-                case "auth/email-already-in-use":
-                case "auth/invalid-email":
-                    console.log(err.message);
-                    setEmailError(err.message);
-                    break;
-                case "auth/weak-password":
-                    console.log(err.message);
-                    setPasswordError(err.message);
-                    break;
-            }
-        }});
-        
-    }
-    
-    const[confirmPassword,setConfirmPassword] = useState("");
-    const [user, setUser] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [emailError, setEmailError] = useState("");
-    const [passwordError, setPasswordError] = useState("");
-    const [hasAccount, setHasAccount] = useState(false);
-    const clearInputs = () =>{
-        setEmail("");
-        setPassword("");
-        setConfirmPassword("");
-    }
-    const clearErrors = () =>{
-        setEmailError("");
-        setPasswordError("");
-    }
-    const authListener = () =>{
-        firebase.auth().onAuthStateChanged((user) =>{
-            if (user){
-                clearInputs();
-                setUser(user);
-            } else {
-                setUser("");
-            }
-        })
-    }
+  function handleEmailLogIn() {
+    clearErrors();
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((err) => {
+        switch (err.code) {
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(err.message);
+            break;
+        }
+      });
+  }
 
-    useEffect(() => {
-        authListener();
-    }, []);
+  function handleSignUp() {
+    clearErrors();
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        if (confirmPassword !== password) {
+          setPasswordError("Passwords do not match");
+        } else {
+          switch (err.code) {
+            case "auth/email-already-in-use":
+            case "auth/invalid-email":
+              console.log(err.message);
+              setEmailError(err.message);
+              break;
+            case "auth/weak-password":
+              console.log(err.message);
+              setPasswordError(err.message);
+              break;
+          }
+        }
+      });
+  }
 
-    return (
-        user ? (<button className="btn btn-lg btn-outline-dark" onClick={()=>{firebase.app().auth().signOut()}}>
-        Log Out
-      </button> ) : (
-        hasAccount ? (
-        <LoginPage 
-        email = {email}
-        setEmail= {setEmail}
-        password = {password}
-        setPassword = {setPassword}
-        handleEmailLogIn = {handleEmailLogIn}
-        handleSignUp = {handleSignUp}
-        hasAccount = {hasAccount}
-        setHasAccount = {setHasAccount}
-        emailError = {emailError}
-        passwordError = {passwordError}
-        confirmPassword = {confirmPassword}
-        setConfirmPassword={setConfirmPassword}> </LoginPage>
-        ) : (
-        <RegisterPage 
-        email = {email}
-        setEmail= {setEmail}
-        password = {password}
-        setPassword = {setPassword}
-        handleEmailLogIn = {handleEmailLogIn}
-        handleSignUp = {handleSignUp}
-        hasAccount = {hasAccount}
-        setHasAccount = {setHasAccount}
-        emailError = {emailError}
-        passwordError = {passwordError}
-        confirmPassword = {confirmPassword}
-        setConfirmPassword={setConfirmPassword}> </RegisterPage>
-        ))
-    );
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [hasAccount, setHasAccount] = useState(false);
+  const clearInputs = () => {
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+  };
+  const clearErrors = () => {
+    setEmailError("");
+    setPasswordError("");
+  };
+  const authListener = () => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        clearInputs();
+        setUser(user);
+      } else {
+        setUser("");
+      }
+    });
+  };
+
+  useEffect(() => {
+    authListener();
+  }, []);
+
+  return user ? (
+    <button
+      className="btn btn-lg btn-outline-dark"
+      onClick={() => {
+        firebase.app().auth().signOut();
+      }}
+    >
+      Log Out
+    </button>
+  ) : hasAccount ? (
+    <LoginPage
+      email={email}
+      setEmail={setEmail}
+      password={password}
+      setPassword={setPassword}
+      handleEmailLogIn={handleEmailLogIn}
+      handleSignUp={handleSignUp}
+      hasAccount={hasAccount}
+      setHasAccount={setHasAccount}
+      emailError={emailError}
+      passwordError={passwordError}
+      confirmPassword={confirmPassword}
+      setConfirmPassword={setConfirmPassword}
+    >
+      {" "}
+    </LoginPage>
+  ) : (
+    <RegisterPage
+      email={email}
+      setEmail={setEmail}
+      password={password}
+      setPassword={setPassword}
+      handleEmailLogIn={handleEmailLogIn}
+      handleSignUp={handleSignUp}
+      hasAccount={hasAccount}
+      setHasAccount={setHasAccount}
+      emailError={emailError}
+      passwordError={passwordError}
+      confirmPassword={confirmPassword}
+      setConfirmPassword={setConfirmPassword}
+    >
+      {" "}
+    </RegisterPage>
+  );
 }
 
-export default LoginSystem
+export default LoginSystem;
