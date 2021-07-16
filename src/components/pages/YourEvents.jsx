0aefec624cff84abfdcff261eacd391289a75c2d
Joseph Nathanael <josephnathanael.2014@gmail.com>
2021-07-16 10:23:16 +0800

    your events page

--- src/components/pages/YourEvents.jsx
+++ src/components/pages/YourEvents.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import useCollections from "../../hooks/useCollections.js";
+import { useAuth } from "../../contexts/AuthContext.js";
+import EventCard from "../dashboard/EventCard";
+import { Container, Row, Col } from "react-bootstrap";
 
 function YourEvents() {
+    const collections = useCollections("events");
+    const { currentUser } = useAuth(); // Authentication Context
+
+    if (!collections) {
+        return;
+      }
+    
+    const eventList = collections.data;
+
+    function yourEventFilter(list){
+        return list.includes(currentUser.uid);
+    }
+
     return (
-        <div>
-            Your Events
+        <div className="home">
+            <Container>
+            <Row className="align-items-center">
+            {eventList.filter((event) => event.participants.includes(currentUser.uid)) // include yourEventFilter here
+            .map((event) => {
+                return (
+                  <Col xs={12} sm={6} md={3}>
+                    <EventCard
+                      sport={event.sport}
+                      place={event.place}
+                      time={event.time}
+                      date={event.date}
+                      quota={event.quota}
+                      imgSrc={event.imgSrc}
+                    />
+                  </Col>
+                );
+              })}
+              </Row>
+              </Container>
         </div>
     )
 }
