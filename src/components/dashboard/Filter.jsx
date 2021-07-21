325baab03ae000a6ae6e56754bba8042f61862dc
Joseph Nathanael <josephnathanael.2014@gmail.com>
2021-07-21 22:50:34 +0800

    errors in sorting feature

--- src/components/dashboard/Filter.jsx
+++ src/components/dashboard/Filter.jsx
@@ -3,8 +3,9 @@ import { Dropdown, Button } from "react-bootstrap";
 
 import "./dashboard.css";
 
-export default function Filter({ setSport }) {
+export default function Filter({ setSport , comparator, setComparator}) {
   const [filterSports, setFilterSports] = useState([]);
+  // const [sorter, setSorter] = useState([]);
 
   function removeFilter(event) {
     const key = event.target.name;
@@ -25,6 +26,84 @@ export default function Filter({ setSport }) {
     }
   }
 
+  function changeSorter(event){
+    const key = event.target.innerText;
+    if(key === "date low-high"){
+      setComparator(sortingDateComparator);
+    }
+    if(key === "date high-low"){
+      setComparator(sortingDateComparatorReverse);
+    }
+    if(key === "players joined low-high"){
+      setComparator(sortingPlayersJoinedComparator);
+    }
+    if(key === "players joined high-low"){
+      setComparator(sortingPlayersJoinedComparatorReverse);
+    }
+    if(key === "quota low-high"){
+      setComparator(sortingQuotaComparator);
+    }
+    if(key === "quota high-low"){
+      setComparator(sortingQuotaComparatorReverse);
+    }
+  }
+
+  function sortingDateComparator(e1, e2) {
+    if(Date.parse(e1.date) > Date.parse(e2.date)){
+      return 1;
+    } else if (Date.parse(e1.date) === Date.parse(e2.date)){
+      let t1_hr = parseInt(e1.time[0] + e1.time[1]);
+      let t1_min = parseInt(e1.time[3] + e1.time[4]);
+      let t2_hr = parseInt(e2.time[0] + e2.time[1]);
+      let t2_min = parseInt(e2.time[3] + e2.time[4]);
+        if(t1_hr > t2_hr){
+          return 0;
+        } else if (t1_hr === t2_hr){
+          if(t1_min > t2_min){
+            return 1;
+          } else {
+            return -1
+          }
+        } else {
+          return -1;
+        }
+    } else {
+      return -1;
+    }
+  }
+
+  function sortingDateComparatorReverse(e1, e2){
+    return -(sortingDateComparator(e1,e2));
+  }
+
+  function sortingPlayersJoinedComparator(e1, e2){
+    console.log("e1:" + e1);
+    console.log("e2:" + e2);
+    console.log("comparator:" + comparator);
+    console.log()
+    if(e1.participants.length > e2.participants.length){
+      return 1;
+    } else {
+      return -1;
+    }
+  }
+
+  function sortingPlayersJoinedComparatorReverse(e1,e2){
+    return -sortingPlayersJoinedComparator(e1,e2);
+  }
+
+  function sortingQuotaComparator(e1, e2){
+    if(e1.quota > e2.quota){
+      return 1;
+    } else {
+      return -1;
+    }
+  }
+
+  function sortingQuotaComparatorReverse(e1,e2){
+    return -sortingQuotaComparator(e1,e2);
+  }
+
   return (
     <div>
       <div className="filter-bar">
@@ -34,10 +113,12 @@ export default function Filter({ setSport }) {
           </Dropdown.Toggle>
 
           <Dropdown.Menu>
-            <Dropdown.Item>date low-high</Dropdown.Item>
-            <Dropdown.Item>date high-low</Dropdown.Item>
-            <Dropdown.Item>players joined low-high</Dropdown.Item>
-            <Dropdown.Item>players joined high-low</Dropdown.Item>
+            <Dropdown.Item onClick = {changeSorter}>date low-high</Dropdown.Item>
+            <Dropdown.Item onClick = {changeSorter}>date high-low</Dropdown.Item>
+            <Dropdown.Item onClick = {changeSorter}>players joined low-high</Dropdown.Item>
+            <Dropdown.Item onClick = {changeSorter}>players joined high-low</Dropdown.Item>
+            <Dropdown.Item onClick = {changeSorter}>quota low-high</Dropdown.Item>
+            <Dropdown.Item onClick = {changeSorter}>quota high-low</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
 
