9c0d3d39078f1bb8c605af11f61aa0bad8f9820a
Joseph Nathanael <josephnathanael.2014@gmail.com>
2021-07-22 14:06:02 +0800

    sorting and filter in your event

--- src/components/pages/YourEvents.jsx
+++ src/components/pages/YourEvents.jsx
@@ -4,66 +4,71 @@
  * The page where users can view their events
  */
 
- import React from "react";
- import { useHistory } from "react-router";
- import "../dashboard/dashboard.css";
- import useCollections from "../../hooks/useCollections.js";
- import { useAuth } from "../../contexts/AuthContext";
- 
- import EventCard from "../dashboard/EventCard";
- 
- export default function YourEvents() {
-   const { currentUser } = useAuth();
-   const collections = useCollections("events");
-   const history = useHistory(); // redirect page
- 
-   function sortingComparator(e1, e2) {
-     return Date.parse(e1.date) > Date.parse(e2.date);
-   }
- 
-   if (!collections) {
-     return <h1>Sorry we have no events for you yet 😓</h1>;
-   }
- 
-   function redirectToEvent(event) {
-     history.push({
-       pathname:"/event",
-       state: event
-     })
-   }
- 
-   function checkIfJoined(participants) {
-     return participants && participants.includes(currentUser.uid);
-   }
- 
-   var eventList = collections.data;
-   eventList = eventList.sort(sortingComparator);
+ import React, { useState } from "react";
+import { useHistory } from "react-router";
+import "../dashboard/dashboard.css";
+import useCollections from "../../hooks/useCollections.js";
+import { useAuth } from "../../contexts/AuthContext";
 
-   eventList = eventList.filter((event) => 'participants' in event && event.participants.includes(currentUser.uid))
+import EventCard from "../dashboard/EventCard";
+import Filter from "../dashboard/Filter";
 
-   if (eventList.length === 0) {
-    return <h1>You haven't joined any event 😓</h1>;
+export default function Home() {
+  const { currentUser } = useAuth();
+  const collections = useCollections("events");
+  const history = useHistory(); // redirect page
+  const [filterSport, setFilterSport] = useState([]);
+  const [comparator, setComparator] = useState(undefined); //default sorter 
+
+  if (!collections) {
+    return <h1>Sorry we have no events for you yet 😓</h1>;
   }
- 
-   return (
-     <div className="wrapper">
-       {eventList.filter((event) => 'participants' in event && event.participants.includes(currentUser.uid)).map((event, id) => {
-         return (
-           <div onClick={() => redirectToEvent(event)} key={id}> 
-             <EventCard
-               key={id}
-               img={event.imgSrc}
-               title={event.title}
-               sport={event.sport}
-               venue={event.place}
-               date={event.date}
-               time={event.time}
-               quota={event.participants.length + " / " + event.quota}
-               blocked={checkIfJoined(event.participants)}
-             />
-           </div>
-         );
-       })}
-     </div>
-   );
- }
+
+  function redirectToEvent(event) {
+    history.push({
+      pathname: "/event",
+      state: event,
+    });
+  }
+
+  function checkIfJoined(participants) {
+    return participants && participants.includes(currentUser.uid);
+  }
+
+  var eventList = collections.data;
+  eventList = eventList.sort(comparator);
+  console.log(eventList);
+
+  if(filterSport.length !== 0) {
+    eventList = eventList.filter(event => filterSport.includes(event.sport))
+  }
+
+  return (
+    <div>
+      <Filter setSport={setFilterSport} comparator = {comparator} setComparator={setComparator} />
+      <div className="wrapper">
+        {eventList.filter((event) => 'participants' in event && event.participants.includes(currentUser.uid)).map((event, id) => {
+          return (
+            <div
+              className={checkIfJoined(event.participants) ? "greyCard" : null}
+              onClick={() => redirectToEvent(event)}
+              key={id}
+            >
+              <EventCard
+                key={id}
+                img={event.imgSrc}
+                title={event.title}
+                sport={event.sport}
+                venue={event.place}
+                date={event.date}
+                time={event.time}
+                quota={event.participants.length + " / " + event.quota}
+                blocked={checkIfJoined(event.participants)}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
