61cd1516fdd8ec5c4249f07c6f66c29012499b08
aryagiat <arya.giat@gmail.com>
2021-07-24 00:21:18 +0800

    Fix: revert private route to the old one

--- src/components/misc/PrivateRoute.jsx
+++ src/components/misc/PrivateRoute.jsx
@@ -7,55 +7,21 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
-import useCollections from "../../hooks/useCollections";
 
 export default function PrivateRoute({ component: Component, ...rest }) {
   /**
    * The PrivateRoute (only for logged in users) component
    */
   const { currentUser } = useAuth(); // Authentication Context
-  const collections = useCollections("users");
-
-  function userHasProfile() {
-    if (!collections) {
-      alert("Failed to retrieve user information");
-    }
-    var users = collections.data;
-    var user = users.filter((user) => user.uid === currentUser.uid);
-    return user.length !== 0;
-  }
-
-  // Get user status
-  function getUserStatus() {
-    /**
-     * Gets the status of the user.
-     *
-     * Returns:
-     * 0 if user is not logged in
-     * 1 if user is logged in
-     * 2 if user is logged in but haven't set up their profile
-     */
-    if (!currentUser) {
-      return 0;
-    } else if (userHasProfile()) {
-      return 1;
-    } else {
-      return 2;
-    }
-  }
-
-  const userStatus = getUserStatus();
 
   return (
     <Route
       {...rest}
       render={(props) => {
-        return userStatus === 0 ? (
-          <Redirect to={"/landing"} />
-        ) : userStatus === 1 ? (
+        return currentUser ? (
           <Component {...props} />
         ) : (
-          <Redirect to={"/edit-profile"} />
+          <Redirect to={"/landing"} />
         );
       }}
     ></Route>
